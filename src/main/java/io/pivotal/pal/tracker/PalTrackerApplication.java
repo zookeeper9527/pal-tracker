c62fbd764a4d7ff0ef294e8a09fc618a84ff6805
pivotal_user <haonan0312@gmail.com>
2019-10-02 14:45:43 -0400

    Autowire jdbctemplate

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -2,6 +2,7 @@ package io.pivotal.pal.tracker;
 
 
 import com.mysql.cj.jdbc.MysqlDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,14 +13,17 @@ import javax.sql.DataSource;
 public class PalTrackerApplication {
 
 
+    @Autowired
+    TimeEntryRepository timeEntryRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
-    @Bean
-    TimeEntryRepository timeEntryRepository(DataSource dataSource){
-        return new JdbcTimeEntryRepository(dataSource);
-    }
+    //@Bean
+   // TimeEntryRepository timeEntryRepository(DataSource dataSource){
+    //    return new JdbcTimeEntryRepository(dataSource);
+    //}
 
 }
 
--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -1,6 +1,7 @@
 package io.pivotal.pal.tracker;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class TimeEntryController {
 
+    @Autowired
     TimeEntryRepository timeEntryRepository;
+
     public TimeEntryController(TimeEntryRepository timeEntryRepository) {
         this.timeEntryRepository = timeEntryRepository;
     }

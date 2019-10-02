b5079d8de56c8b7d85de074a67a170999b46a6a1
pivotal_user <haonan0312@gmail.com>
2019-10-02 15:05:50 -0400

    remove Autowire jdbctemplate

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -2,7 +2,6 @@ package io.pivotal.pal.tracker;
 
 
 import com.mysql.cj.jdbc.MysqlDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -13,17 +12,14 @@ import javax.sql.DataSource;
 public class PalTrackerApplication {
 
 
-    @Autowired
-    TimeEntryRepository timeEntryRepository;
-
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
-    //@Bean
-   // TimeEntryRepository timeEntryRepository(DataSource dataSource){
-    //    return new JdbcTimeEntryRepository(dataSource);
-    //}
+    @Bean
+    TimeEntryRepository timeEntryRepository(DataSource dataSource){
+        return new JdbcTimeEntryRepository(dataSource);
+    }
 
 }
 
